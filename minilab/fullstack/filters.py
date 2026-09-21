68950415475ce3b4802c704aaf51db212783e8df
AudreyFitchett <audreyfitchett@u.boisestate.edu>
2026-09-20 18:54:10 -0600

    cleaned up filters and deleted detail.html. Still works

--- minilab/fullstack/filters.py
+++ minilab/fullstack/filters.py
@@ -1,20 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
-from .models import Assignment, Course
-from django.views.generic import ListView
-import django_tables2 as tables
-from rest_framework import serializers
-from django.http import Http404
-from django.urls import reverse
-from django.db.models import F
-from django.views import generic
-from django.http import JsonResponse
-from django.views import View
-from .services import ExternalApiClient, ExternalAPIServiceError
+from .models import Assignment
 from django_filters import FilterSet
-
 
 class AssignmentFilter(FilterSet):
     class Meta:
