6aace2eb5447a5a6329c6cc91e15d38709d22e38
AudreyFitchett <audreyfitchett@u.boisestate.edu>
2026-09-20 18:37:56 -0600

    cleaned up tables.py - everything still workds

--- minilab/fullstack/views.py
+++ minilab/fullstack/views.py
@@ -1,19 +1,7 @@
-from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
 from .models import Assignment, Course
-from django.views.generic import ListView
-import django_tables2 as tables
-from rest_framework import serializers
-from django.http import Http404
-from django.urls import reverse
-from django.db.models import F
 from django.views import generic
-from django.http import JsonResponse
-from django.views import View
 from .services import ExternalApiClient, ExternalAPIServiceError
-from django_tables2 import SingleTableView
 from .tables import AssignmentTable
 from .filters import AssignmentFilter
 from django_filters.views import FilterView
